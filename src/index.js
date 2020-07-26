2bd8ffe8c3bcb99996e16aa91ed5c2606aa79832
kgalaszewski95@gmail.com <kgalaszewski95@gmail.com>
2020-07-26 20:41:03 +0200

    First task resolved, deleted some unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// TO RUN ONE OF 4 examples, comment other three examples to not override them one by one
+// ##1 React.createElement
+const myHeader = React.createElement(
+  'h1',
+  { className: 'header_title'},
+  'Some title generated using createElement'
+)
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(myHeader, rootElement)
+
+
+// ##2 React.Component
+class SecondExample extends React.Component {
+  render() {
+    return <h1>Second title generated using ReactComponent</h1>;
+  }
+}
+
+ReactDOM.render(<SecondExample/>, document.getElementById('root'))
+
+
+// ##3 React.PureComponent
+class ThirdExample extends React.PureComponent {
+  render() {
+    return <h1>Third title generated using ReactPureComponent</h1>;
+  }
+}
+
+ReactDOM.render(<ThirdExample/>, document.getElementById('root'))
+
+
+// ##4 Functional Component
+function GenerateFourthTitle()
+{
+  return <h1>Fourth title generated using functional component</h1>;
+}
+
+ReactDOM.render(
+  <GenerateFourthTitle/>,
+  document.getElementById('root')
+)
+// export default GenerateFourthTitle;
+
+
+// the end
+
 serviceWorker.unregister();
